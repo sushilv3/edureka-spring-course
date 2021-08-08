85a1f7a3aa1a5cb617d66519a1105ba1853b9793
sushilv3 <sushilv3rma@gmail.com>
2021-08-08 13:17:30 +0530

    edureka spring course project intialize with intellij ide

---
+++ src/main/java/edureka/springcourse/EdurekaSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package edureka.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurekaSpringCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdurekaSpringCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edureka/springcourse/EdurekaSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package edureka.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurekaSpringCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
